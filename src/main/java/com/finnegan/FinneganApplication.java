a2cc78b60cdfc4a9e3b74cd69c1c6f8bc740ce0e
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-04-24 00:50:20 -0400

    initial commit

---
+++ src/main/java/com/finnegan/FinneganApplication.java
@@ -0,0 +1,13 @@
+package com.finnegan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinneganApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinneganApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finnegan/FinneganApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finnegan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinneganApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
