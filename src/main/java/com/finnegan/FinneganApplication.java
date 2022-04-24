afda09a4836e30154bf8706f650c1e136e660a65
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-04-24 01:06:23 -0400

    set up logger

--- src/main/java/com/finnegan/FinneganApplication.java
+++ src/main/java/com/finnegan/FinneganApplication.java
@@ -2,12 +2,18 @@ package com.finnegan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @SpringBootApplication
 public class FinneganApplication {
+    private static final Logger logger =
+            LoggerFactory.getLogger(FinneganApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(FinneganApplication.class, args);
+        logger.info("Hello Spring Boot");
     }
 
 }
