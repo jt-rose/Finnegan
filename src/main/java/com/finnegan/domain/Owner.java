8f03de212030bb07a34dce45c6af6afac7f28946
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-04-25 18:57:17 -0400

    set up get mapping

--- src/main/java/com/finnegan/domain/Owner.java
+++ src/main/java/com/finnegan/domain/Owner.java
@@ -1,9 +1,13 @@
 package com.finnegan.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "owners")
 public class Owner {
     @Id
@@ -21,6 +25,7 @@ public class Owner {
     private boolean active;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    @JsonIgnore
     private List<Transaction> transactions;
 
     public Owner(String email, String password) {
---
+++ src/main/java/com/finnegan/web/UserController.java
@@ -0,0 +1,18 @@
+package com.finnegan.web;
+
+import com.finnegan.domain.Transaction;
+import com.finnegan.domain.TransactionRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    private TransactionRespository transactionRepo;
+
+    @RequestMapping("/")
+    public Iterable<Transaction> getTransactions() {
+        return transactionRepo.findAll();
+    }
+}
