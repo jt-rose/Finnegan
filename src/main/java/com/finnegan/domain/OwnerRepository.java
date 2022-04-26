84d60c909e43b9983124ec01311201d20b04609f
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-04-25 22:11:02 -0400

    set up automated REST endpoints

--- src/main/java/com/finnegan/domain/OwnerRepository.java
+++ src/main/java/com/finnegan/domain/OwnerRepository.java
@@ -1,6 +1,12 @@
 package com.finnegan.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import java.util.List;
 
+@RepositoryRestResource
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    // fetch owner by email
+    List<Owner> findByEmail(@Param("email") String email);
 }
--- src/main/java/com/finnegan/web/OwnerController.java
+++ src/main/java/com/finnegan/web/OwnerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserController {
+public class OwnerController {
     @Autowired
     private TransactionRespository transactionRepo;
 
